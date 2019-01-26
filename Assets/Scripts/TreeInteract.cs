83679cd37069377f31e9a6c175269b33721fa81e
hitechlife <kanosmask@gmail.com>
2019-01-26 11:43:33 -0500

    updated scripts

--- Assets/Scripts/TreeInteract.cs
+++ Assets/Scripts/TreeInteract.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class TreeInteract : MonoBehaviour
 {
     // Start is called before the first frame update
+    List<string> enemies;
     void Start()
     {
-        
+        //add types of enemy tags here
+        enemies.Add("lumberjack");
     }
 
     // Update is called once per frame
@@ -17,5 +19,11 @@ public class TreeInteract : MonoBehaviour
         //when killing an enemy, increasee score
         //when enemy enters trunk trigger, trigger animation with animals (and delay movements) -- when death zone entered, destroy both gameobjects
         //deal damage to tree trunk if reached (trigger game over, STOP game)
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (enemies.Contains(other.tag)) {
+
+        }
     }
 }
