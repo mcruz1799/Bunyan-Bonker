5afc7ec21d178fdb2ca82f40cd6f5cb4bd4b11ce
hitechlife <kanosmask@gmail.com>
2019-01-26 11:25:07 -0500

    updated scripts

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -22,7 +22,7 @@ public class EnemyMover : MonoBehaviour
             speed = 0.05f * level;
             //assuming tree is at 0, check which side we are on
             if(this.transform.position.x > 0) speed = -speed;
-        } 
+        }
         else{
             speed = 0.02f * level;
             if(this.transform.position.x > 0) speed = -speed;
@@ -31,11 +31,6 @@ public class EnemyMover : MonoBehaviour
 
    void Update()
     {
-        //die if touches tree ONLY IF tree is slamming [tree script - OnTriggerEnter?]
-        //does this enemy's death increase score? [again tree script]
-        //interact with animals (and delay movements) [animal script - enemy rigidbodies] -- when trigger entered, destroy both gameobjects
-        //deal damage to tree trunk if reached (trigger game over, STOP game) [tree script]
-
         //assuming we're in the x-y plane, move in x direction
         if (moving) {
             this.transform.Translate(new Vector3(speed, 0, 0));
