bc9f1cbc499e749e38f0b15758210a906d216d07
hitechlife <kanosmask@gmail.com>
2019-01-26 11:51:42 -0500

    updated tree interact script

--- Assets/Scripts/TreeInteract.cs
+++ Assets/Scripts/TreeInteract.cs
@@ -6,24 +6,45 @@ public class TreeInteract : MonoBehaviour
 {
     // Start is called before the first frame update
     List<string> enemies;
+    bool slamming;
+    int score;
+    public int lives = 3;
+    public Transform animal;
+    public int interval = 10; //
     void Start()
     {
         //add types of enemy tags here
         enemies.Add("lumberjack");
+
+        slamming = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //TODO: kill enemy if touches tree ONLY IF tree is slamming AND enemy is not being animated
-        //when killing an enemy, increasee score
-        //when enemy enters trunk trigger, trigger animation with animals (and delay movements) -- when death zone entered, destroy both gameobjects
-        //deal damage to tree trunk if reached (trigger game over, STOP game)
+
     }
 
+    //if someone enters safe zone
     void OnTriggerEnter(Collider other){
         if (enemies.Contains(other.tag)) {
+            if (lives > 0){
+                //instantiate animal and trigger animation
+            }
+            else{
+                //game over
+            }
+        }
+    }
 
+    void OnColliderEnter(Collider other){
+        if (enemies.Contains(other.tag)) {
+            if (slamming /* && enemy is not being animated */) {
+                Destroy(other.gameObject);
+
+                //increase score
+                score += interval;
+            }
         }
     }
 }
