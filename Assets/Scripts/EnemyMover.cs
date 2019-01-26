776c09dfee0e4adc668340c7cd86599a66ffac9b
Matt Cruz <mruz1@andrew.cmu.edu>
2019-01-25 23:11:22 -0500

    I'm a stoopid idiot

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossMover : MonoBehaviour
+public class EnemyMover : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
